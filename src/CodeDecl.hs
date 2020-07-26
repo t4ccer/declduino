ab846bec296dbe886bb5ea215e3fa34797f66f8c
t4ccer <64430288+t4ccer@users.noreply.github.com>
2020-07-26 20:01:00 +0200

    Add simple eDSL for Arduino code

--- src/CodeDecl.hs
+++ src/CodeDecl.hs
@@ -5,7 +5,7 @@ module CodeDecl where
 import CodeGen
 import GHC.Generics
 import Data.Text ()
-import Data.Yaml
+import Data.Yaml hiding ((.:))
 
 
 data Device = Device
@@ -39,33 +39,32 @@ instance ToJSON Component
 
 deviceToCode :: Device -> [CodeToken]
 deviceToCode dev
-    | board dev == "esp32" =
-        [Include "<WiFi.h>"
-        ,Include "<PubSubClient.h>"
+    | board dev == "esp32" = getTokens $ start
+        .: Include "<WiFi.h>"
+        .: Include "<PubSubClient.h>"
 
-        ,VarDecl "WiFiClient" "espClient" [],Semicolon,NL
-        ,VarDecl "PubSubClient" "client" [Variable "espClient"],Semicolon,NL]
+        .: VarDecl "WiFiClient" "espClient" [] .: Semicolon
+        .: VarDecl "PubSubClient" "client" [Variable "espClient"] .: Semicolon
 
-        ++funcs++
+        <: funcs
         
-        [Function Void "callback" [Argument "char*" "topic",Argument "byte*" "message", Argument "unsigned int" "length"] 
-            conds
+        .: Function Void "callback" [Argument "char*" "topic",Argument "byte*" "message", Argument "unsigned int" "length"] conds
 
-        ,Function Void "setup" [] 
-            ([Call "WiFi.begin" [StringLit ssid', StringLit pass'],Semicolon,NL
-            ,WhileLoop [Call "WiFi.status" [], Op NotEquals, Value (Variable "WL_CONNECTED")] 
-                [Call "delay" [IntLit 500],Semicolon,NL]    
-            ,Call "client.setServer" [StringLit mqtt', IntLit port'],Semicolon,NL
-            ,Call "client.setCallback" [Variable "callback"],Semicolon,NL
-            ,Call "client.connect" [StringLit "esp32"],Semicolon,NL
-            ]++
-            subs++
-            pins)
-        ,Function Void "loop" []
-            [Call "client.loop" []
-            ,Semicolon
-            ]
-        ]
+        .: Function Void "setup" []  (getTokens (start
+            .: Call "WiFi.begin" [StringLit ssid', StringLit pass'] .: Semicolon
+            .: WhileLoop [Call "WiFi.status" [], Op NotEquals, Value (Variable "WL_CONNECTED")] (getTokens (start
+                .: Call "delay" [IntLit 500] .: Semicolon .: NL))
+                
+            .: Call "client.setServer" [StringLit mqtt', IntLit port'] .: Semicolon
+            .: Call "client.setCallback" [Variable "callback"] .: Semicolon
+            .: Call "client.connect" [StringLit "esp32"] .: Semicolon
+            <: subs
+            <: pins))
+        .: Function Void "loop" [] (getTokens (start
+            .: Call "client.loop" []
+            .: Semicolon))
+            
+        
     | otherwise = error "Unsupported board"
     where
         ssid' = ssid dev
@@ -91,14 +90,15 @@ componentToCallbackCond dev comp =
 componentToFunc :: Component -> CodeToken
 componentToFunc comp
     | componentType comp == DigitalOutput = 
-        Function Void funcName [Argument "byte*" "message", Argument "unsigned int" "length"] 
-            [If [Value (Variable "length"), Op NotEquals, Value (IntLit 1)] [Return []]
-                
-            ,If [Value (Variable "message[0]"), Op Equals, Value (CharLit '1')] 
-                [Call "digitalWrite" [IntLit pin', Variable "HIGH"],Semicolon,NL]
-            ,Else [If [Value (Variable "message[0]"), Op Equals, Value (CharLit '0')] 
-                [Call "digitalWrite" [IntLit pin', Variable "LOW"],Semicolon,NL]]
-            ]
+        Function Void funcName [Argument "byte*" "message", Argument "unsigned int" "length"] (getTokens (start
+            .: If [Value (Variable "length"), Op NotEquals, Value (IntLit 1)] 
+                [Return []]
+            .: If [Value (Variable "message[0]"), Op Equals, Value (CharLit '1')] (getTokens (start
+                .: Call "digitalWrite" [IntLit pin', Variable "HIGH"] .: Semicolon .: NL))
+            .: Else (getTokens (start 
+                .:If [Value (Variable "message[0]"), Op Equals, Value (CharLit '0')] (getTokens (start
+                    .: Call "digitalWrite" [IntLit pin', Variable "LOW"] .: Semicolon .: NL))))
+            ))
     | otherwise = error "Unsupported device"
         where
             pin' = pin comp
@@ -106,10 +106,15 @@ componentToFunc comp
 
 componentToPinMode :: Component -> [CodeToken]
 componentToPinMode comp 
-    | componentType comp == DigitalOutput = [Call "pinMode" [IntLit (pin comp), Variable "OUTPUT"],Semicolon,NL]
+    | componentType comp == DigitalOutput = getTokens $ start
+         .: Call "pinMode" [IntLit (pin comp), Variable "OUTPUT"] 
+         .: Semicolon 
+         .: NL
+    
     | otherwise = error "Unsopported device"
 
 componentToSubs :: Device -> Component -> [CodeToken]
 componentToSubs dev comp 
-    | componentType comp == DigitalOutput = [Call "client.subscribe" [StringLit ("declduino/"++devName dev++"/"++compName comp)],Semicolon,NL]
+    | componentType comp == DigitalOutput = getTokens $ start
+        .: Call "client.subscribe" [StringLit ("declduino/"++devName dev++"/"++compName comp)] .: Semicolon .: NL
     | otherwise = error "Unsopported device"
