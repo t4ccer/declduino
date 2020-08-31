195c5fed4d23228d325c9418d4dd47dc9ab7f614
t4ccer <64430288+t4ccer@users.noreply.github.com>
2020-08-31 13:26:37 +0200

    Add IOLoggerResult monad

--- src/LoggerResult.hs
+++ src/LoggerResult.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DatatypeContexts #-}
-
 module LoggerResult where
 
 data LogLevel = 
@@ -15,6 +13,12 @@ data Log = Log LogLevel String
 
 data LoggerResult a = LoggerResult [Log] (Maybe a)
     deriving Show
+
+class Logger l where
+    returnError   :: Log -> l a
+    returnWithLog :: Log -> a -> l a
+    appendLogs    :: [Log] -> l a -> l a
+    tell          :: l a -> l [Log]
 
 instance Functor LoggerResult where
     fmap f (LoggerResult w m) = LoggerResult w $ fmap f m
@@ -35,20 +39,40 @@ instance Monad LoggerResult where
             where 
                 LoggerResult w' v'' = f v'
 
-mkResult :: a -> LoggerResult a
-mkResult = return
+instance Logger LoggerResult where
+    returnError e = LoggerResult [e] Nothing
+    returnWithLog l v = LoggerResult [l] $ Just v
+    tell (LoggerResult w _) = LoggerResult w $ Just w
+    appendLogs ls (LoggerResult w v) = LoggerResult (w++ls) v
 
-returnError :: Log -> LoggerResult a
-returnError e = LoggerResult [e] Nothing
+appendLog :: Logger l => Log -> l a -> l a
+appendLog l = appendLogs [l] 
 
-returnWithLog :: Log -> a -> LoggerResult a
-returnWithLog l v = LoggerResult [l] $ Just v
 
-appendLog :: Log -> LoggerResult a -> LoggerResult a
-appendLog l (LoggerResult w v) = LoggerResult (w++[l]) v
+newtype IOLoggerResult a = IOLoggerResult (LoggerResult (IO a))
 
-appendLogs :: [Log] -> LoggerResult a -> LoggerResult a
-appendLogs ls (LoggerResult w v) = LoggerResult (w++ls) v
+instance Functor IOLoggerResult where
+    fmap f (IOLoggerResult (LoggerResult w io))= IOLoggerResult (LoggerResult w $ fmap (fmap f) io)
 
-tell :: LoggerResult a -> LoggerResult [Log]
-tell (LoggerResult w _) = LoggerResult w $ Just w
+instance Applicative IOLoggerResult where
+    pure v = IOLoggerResult (LoggerResult [] (return $ return v))
+    (IOLoggerResult (LoggerResult w f)) <*> (IOLoggerResult (LoggerResult w' v)) = case f of
+        Nothing -> IOLoggerResult (LoggerResult (w++w') Nothing)
+        Just f' -> case v of
+            Nothing -> IOLoggerResult $ LoggerResult (w++w') Nothing
+            Just v' -> IOLoggerResult $ LoggerResult (w++w') $ Just $ f' <*> v'
+
+instance Monad IOLoggerResult where
+    return = pure
+    IOLoggerResult (LoggerResult w v) >>= f = case v of
+        Nothing -> IOLoggerResult $ LoggerResult w Nothing
+        Just v' -> fromIO v' >>= f
+
+instance Logger IOLoggerResult where
+    returnError l = IOLoggerResult (LoggerResult [l] Nothing)
+    returnWithLog l v = IOLoggerResult (LoggerResult [l] $ return $ return v)
+    appendLogs ls (IOLoggerResult (LoggerResult w v)) = IOLoggerResult (LoggerResult (ls++w) v)
+    tell (IOLoggerResult (LoggerResult w _)) = return w
+
+fromIO :: IO a -> IOLoggerResult a
+fromIO v = IOLoggerResult (LoggerResult [] $ return v)
