791bad8807e2f8b8764d2767b744d05a6ed12bcb
t4ccer <t4ccer@gmail.com>
2021-07-01 22:55:36 +0200

    Add App skeleton

--- src/Declduino/App.hs
+++ src/Declduino/App.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Declduino.App where
+
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Control.Monad.Writer
+
+type Env = ()
+
+type Log = ()
+
+type Logs = [Log]
+
+type Error = ()
+
+newtype App a = App { getApp :: ReaderT Env (WriterT Logs (ExceptT Error IO)) a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadReader Env, MonadWriter Logs, MonadError Error)
+
+addLog :: Log -> App ()
+addLog = undefined
+
+getLogs :: App a -> App Logs
+getLogs = undefined
+
+runApp :: Env -> App a -> IO (Either Error a)
+runApp = undefined
 
--- src/Declduino/Log.hs
+++
@@ -1,2 +0,0 @@
-module Declduino.Log where
-
